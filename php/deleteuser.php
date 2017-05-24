5bf4f235f2bf321e43e4076e165a6d1be04b02b2
Akshay <ksh.gupt1@outlook.com>
2017-05-24 23:35:00 +0530

    Add files via upload

---
+++ php/deleteuser.php
@@ -0,0 +1,23 @@
+<?php
+
+$pin = urldecode($_POST['pin']);
+
+$con = mysqli_connect("localhost","root","lucifer123");
+$x = mysqli_select_db($con,"autoH");
+
+if(!$con){
+	die("Cannot setup connection: ". mysqli_connect_error());
+}
+
+$query = "delete from user where pin='$pin'";
+$result = mysqli_query($con,$query);
+
+if($result){
+	echo "User Successfully Deleted";
+}
+else{
+	echo "Problem in Deletion. Probably coder's fault..";
+}
+
+
+?>
